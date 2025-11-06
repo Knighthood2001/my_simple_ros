ce0b971cb5b25be99595657eb4a877f050caecc6
knighthood <2109695291@qq.com>
2025-11-06 23:42:22 +0800

    消息工厂最初版本：单例模式的基本框架

--- include/msg_factory.h
+++ include/msg_factory.h
@@ -5,4 +5,26 @@
 #ifndef MSG_FACTORY_H
 #define MSG_FACTORY_H
 
+#include <google/protobuf/message.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/dynamic_message.h>
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+class MsgFactory{
+public:
+  // 获取单例实例
+  static MsgFactory& instance();
+private:
+  MsgFactory()=default;
+  ~MsgFactory()=default;
+  // 禁止拷贝和赋值
+  MsgFactory(const MsgFactory&)=delete;
+  MsgFactory& operater=(const MsgFactory&)=delete;
+
+
+
+}
+
 #endif //MSG_FACTORY_H
--- src/msg_factory.cpp
+++ src/msg_factory.cpp
@@ -2,3 +2,8 @@
 // Created by wu on 25-11-6.
 //
 #include "msg_factory.h"
+
+MsgFactory& MsgFactory::instance(){
+  static MsgFactory instance;
+  return instance;
+}
