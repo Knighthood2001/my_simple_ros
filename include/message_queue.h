7708c0af9135493d25f45ce9b3a57f94cdf37676
knighthood <2109695291@qq.com>
2025-11-07 15:06:25 +0800

    设置主题的最大消息队列大小

--- include/message_queue.h
+++ include/message_queue.h
@@ -16,6 +16,12 @@
 class MessageQueue {
 public:
   using Callback = std::function<void(const std::shared_ptr<google::protobuf::Message>&)>; //回调函数
+  // 设置主题的最大消息队列大小
+  void setTopicMaxQueueSize(const std::string& topic, uint32_t max_size){
+    std::lock_guard<std::mutex> lock(mutex_);
+    topic_max_queue_sizes_[topic] = max_size;
+  }
+
   //添加订阅者
   void addSubscriber(const std::string& topic, Callback cb){
     std::lock_guard<std::mutex> lock(mutex_);
@@ -45,5 +51,6 @@ private:
   //多主题消息队列
   std::unordered_map<std::string, std::list<std::shared_ptr<google::protobuf::Message>>> message_queue_;
   std::unordered_map<std::string, std::vector<Callback>> subscribers_; //订阅者列表
+  std::unordered_map<std::string, uint32_t> topic_max_queue_sizes_; //每个主题的最大消息队列大小
 };
 #endif //MESSAGE_QUEUE_H
