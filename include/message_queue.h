f715e8ec0da73697e13ff00523805539a3703022
knighthood <2109695291@qq.com>
2025-11-07 14:42:38 +0800

    实现多主题的消息队列

--- include/message_queue.h
+++ include/message_queue.h
@@ -7,25 +7,29 @@
 #include <list>
 #include <memory>
 #include <mutex>
+#include <unordered_map>
+#include <string>
 
 class MessageQueue {
 public:
-  void push(std::shared_ptr<google::protobuf::Message> msg){
+  // 推送消息到指定主题的队列
+  void push(const std::string& topic, std::shared_ptr<google::protobuf::Message> msg){
     std::lock_guard<std::mutex> lock(mutex_);
-    message_queue_.push_back(msg);
+    message_queue_[topic].push_back(msg);
   }
-  void process(){
+  void process(const std::string& topic){
     std::lock_guard<std::mutex> lock(mutex_);
-    if(!message_queue_.empty()){
-      auto msg = message_queue_.front();
-      message_queue_.pop_front();
+    if(!message_queue_[topic].empty()){
+      auto msg = message_queue_[topic].front();
+      message_queue_[topic].pop_front();
       //处理消息
     }
   }
 
 private:
   std::mutex mutex_;
-  std::list<std::shared_ptr<google::protobuf::Message>> message_queue_;
+  //多主题消息队列
+  std::unordered_map<std::string, std::list<std::shared_ptr<google::protobuf::Message>>> message_queue_;
 
-}
+};
 #endif //MESSAGE_QUEUE_H
