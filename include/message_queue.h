5d729cee76ac4e244138452dcecb74e1c9425f28
knighthood <2109695291@qq.com>
2025-11-07 15:19:26 +0800

    实现:当队列满时，自动移除最早的消息

--- include/message_queue.h
+++ include/message_queue.h
@@ -16,6 +16,11 @@
 class MessageQueue {
 public:
   using Callback = std::function<void(const std::shared_ptr<google::protobuf::Message>&)>; //回调函数
+                                                                                           
+  // 构造函数，可以设置默认队列大小
+  MessageQueue(uint32_t default_max_queue_size = 1000)
+        : default_max_queue_size_(default_max_queue_size) {}
+    
   // 设置主题的最大消息队列大小
   void setTopicMaxQueueSize(const std::string& topic, uint32_t max_size){
     std::lock_guard<std::mutex> lock(mutex_);
@@ -31,6 +36,13 @@ public:
   // 推送消息到指定主题的队列
   void push(const std::string& topic, std::shared_ptr<google::protobuf::Message> msg){
     std::lock_guard<std::mutex> lock(mutex_);
+    //检查队列是否为满，如果满，移除最早的消息
+    auto it = topic_max_queue_sizes_.find(topic);
+    uint32_t max_size = (it != topic_max_queue_sizes_.end()) ? it -> second : default_max_queue_size_;
+    if (message_queue_[topic].size() >= max_size){
+      message_queue_[topic].pop_front();
+    }
+    //添加新消息到队列
     message_queue_[topic].push_back(msg);
   }
   //处理指定主题的消息
@@ -48,6 +60,7 @@ public:
 
 private:
   std::mutex mutex_;
+  uint32_t default_max_queue_size_;                            // 默认队列大小
   //多主题消息队列
   std::unordered_map<std::string, std::list<std::shared_ptr<google::protobuf::Message>>> message_queue_;
   std::unordered_map<std::string, std::vector<Callback>> subscribers_; //订阅者列表
