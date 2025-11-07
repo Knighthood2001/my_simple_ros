868a4b81b576479d0cab11bb3c1594ae01302539
knighthood <2109695291@qq.com>
2025-11-07 14:58:35 +0800

    添加订阅者支持:支持为每个主题添加订阅者，当有新消息时通知订阅者。

--- include/message_queue.h
+++ include/message_queue.h
@@ -9,20 +9,34 @@
 #include <mutex>
 #include <unordered_map>
 #include <string>
+#include <functional>
+#include <vector>
+#include <google/protobuf/message.h>
 
 class MessageQueue {
 public:
+  using Callback = std::function<void(const std::shared_ptr<google::protobuf::Message>&)>; //回调函数
+  //添加订阅者
+  void addSubscriber(const std::string& topic, Callback cb){
+    std::lock_guard<std::mutex> lock(mutex_);
+    subscribers_[topic].push_back(cb);
+  }
+
   // 推送消息到指定主题的队列
   void push(const std::string& topic, std::shared_ptr<google::protobuf::Message> msg){
     std::lock_guard<std::mutex> lock(mutex_);
     message_queue_[topic].push_back(msg);
   }
+  //处理指定主题的消息
   void process(const std::string& topic){
     std::lock_guard<std::mutex> lock(mutex_);
     if(!message_queue_[topic].empty()){
       auto msg = message_queue_[topic].front();
       message_queue_[topic].pop_front();
       //处理消息
+      for (const auto& cb : subscribers_[topic]) {
+        cb(msg);
+      }
     }
   }
 
@@ -30,6 +44,6 @@ private:
   std::mutex mutex_;
   //多主题消息队列
   std::unordered_map<std::string, std::list<std::shared_ptr<google::protobuf::Message>>> message_queue_;
-
+  std::unordered_map<std::string, std::vector<Callback>> subscribers_; //订阅者列表
 };
 #endif //MESSAGE_QUEUE_H
