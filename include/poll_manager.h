52e5212c34a7f082ee12ebf4782ad0f8390d818a
knighthood <2109695291@qq.com>
2025-11-08 23:08:18 +0800

    使用muduo实现基本的TCP服务器

--- include/poll_manager.h
+++ include/poll_manager.h
@@ -8,7 +8,14 @@
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/InetAddress.h>
 #include <string>
-
+/*
+| 成员                       | 作用                 |
+| ------------------------ | ------------------ |
+| `EventLoop* loop`        | Muduo 的事件循环，IO复用核心 |
+| `InetAddress listenAddr` | 服务器监听的 IP + 端口     |
+| `TcpServer server_`      | Muduo 的 TCP 服务器对象  |
+| `onConnection()`         | 连接事件回调函数           |
+ */
 class PollManager{
 public:
   PollManager(muduo::net::EventLoop* loop, const muduo::net::InetAddress& listenAddr);
--- src/poll_manager.cpp
+++ src/poll_manager.cpp
@@ -5,7 +5,7 @@
 #include <muduo/base/Logging.h>
 
 PollManager::PollManager(muduo::net::EventLoop* loop, const muduo::net::InetAddress& listenAddr)
-    : server_(loop, listenAddr, "PollManager"){
+    : server_(loop, listenAddr, "PollManager"){  //设置服务器名称 "PollManager"
   server_.setConnectionCallback(std::bind(&PollManager::onConnection, this, std::placeholders::_1));
 
 }
@@ -15,7 +15,7 @@ void PollManager::start(){
 }
 
 void PollManager::onConnection(const muduo::net::TcpConnectionPtr& conn){
-  if (conn->isConnected()) {
+  if (conn->isConnected()) { // 判断连接状态：是否是新建立的连接
     LOG_INFO << "new connection: " << conn->peerAddress().toIpPort();
   } else {
     LOG_INFO << "connection closed: " << conn->name();
